b05896016790a1e588d082e235f537a429de8a8d
Berkan Demiral <alfonso.brkn@gmail.com>
2021-05-28 23:21:53 +0300

    Edited pom.xml and Users.java

--- src/main/java/javacamp/hrms/entities/concretes/Users.java
+++ src/main/java/javacamp/hrms/entities/concretes/Users.java
@@ -7,6 +7,10 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,14 +22,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor // parametresiz constructor ekledi
 @Inheritance(strategy = InheritanceType.JOINED)
 public class Users {
+	
 	@Id // id alanını belirttik.
 	@GeneratedValue // auto increment(bir bir artırılacak) diye belirttik.
 	@Column(name = "user_id") // hangi sütuna karşılık geliyor.
 	private int id;
 	
-	@Column(name = "email")
-	private String email;
 	
+	@Email(message="Geçerli bir email adresi giriniz")
+	@Column(name = "email")
+	private String email; 
+	
+	@NotBlank(message = "Şifre alanını boş bırakmayınız.")
+	@Size(min=6,max =20 , message="Şifre min 6 max 20 karakterden oluşmalıdır.")
 	@Column(name = "password")
 	private String password;
 	
