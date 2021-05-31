4f8f97d612f166482a2da446cac06ff007c57599
Berkan Demiral <alfonso.brkn@gmail.com>
2021-05-31 23:40:02 +0300

    import change

--- src/main/java/javacamp/hrms/api/controllers/JobPositionsController.java
+++ src/main/java/javacamp/hrms/api/controllers/JobPositionsController.java
@@ -3,12 +3,10 @@ package javacamp.hrms.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javacamp.hrms.buisness.abstracts.JobPositionService;
-import javacamp.hrms.entities.concretes.JobPosition;
 
 @RestController
 @RequestMapping("/api/job_positions") // kodlama.io/api/products diye bir istekte bulunursak bu çalışacak.
