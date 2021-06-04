a15d4ea90a7b32c17376ceae2d74050051e7c3e2
Berkan Demiral <alfonso.brkn@gmail.com>
2021-06-04 13:19:55 +0300

    Job posting and City added

---
+++ src/main/java/javacamp/hrms/entities/concretes/City.java
@@ -0,0 +1,43 @@
+package javacamp.hrms.entities.concretes;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="cities")
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","employer","city"}) // olur da sonsuz döngüye girip fazla sorgu getirirse onu durdurmasını söyleyen kod bloğu
+public class City {
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@ManyToOne()
+	@JoinColumn(name="employer_id")
+	private Employer employer;
+	
+	@ManyToOne()
+	@JoinColumn(name="city_id")
+	private City city;
+	
+	
+}
