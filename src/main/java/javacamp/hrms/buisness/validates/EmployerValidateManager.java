44ff604593722cc1cff83bc2ca2ea62e29c29199
Berkan Demiral <alfonso.brkn@gmail.com>
2021-06-06 23:59:40 +0300

    EmployerValidateManager repaired

--- src/main/java/javacamp/hrms/buisness/validates/EmployerValidateManager.java
+++ src/main/java/javacamp/hrms/buisness/validates/EmployerValidateManager.java
@@ -50,9 +50,9 @@ public class EmployerValidateManager implements ValidateService<Employer>{
 		if(employer.getPassword().equals(employer.getRepeatPassword()) == false) { // 2.kez girilen password ile eşleşmiyorsa
 			return new ErrorResult("Şifreler uyuşmuyor");
 		}
-		if(employerDao.getByCompanyName(employer.getCompanyName()).isUserConfirm()) {
-			--
-		}
+		/*if(employerDao) {
+			
+		}*/
 		
 		this.employerDao.save(employer); // eğer iflerin içine girmediyse doğrulama tamam demektir..
 		this.verifyCodeService.createVerifyCode(employer);
